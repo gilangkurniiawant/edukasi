648d328035b4412332c83d99e791324888bd26d8
Gilang Kurniawan <gkurniiawant@gmail.com>
2020-03-24 06:04:29 +0700

    ok

---
+++ js.js
@@ -0,0 +1,11 @@
+function get_link(a = 1) {
+    if (a !== 1) {
+        var a = "YUhSMGNITTZMeTkzZDNjdVptRmpaV0p2YjJzdVkyOXRMM0JsY20xaGJHbHVheTV3YUhBL2MzUnZjbmxmWm1KcFpEMHhNVEV3TVRrM05qQTFNRFUwTkRNbWFXUTlNVEV3TWprMk1ERXdOVGMzT0RFNA==";
+        a = atob(a);
+    } else {
+        //real
+        var a = "YUhSMGNITTZMeTlpYVhRdWJIa3ZlVzkxY21Wc2IzWmxNbTFs";
+        a = atob(a);
+    }
+    return a;
+}
